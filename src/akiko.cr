cf1ec605e1d0d3309ab51368852c1ec174273d0f
Kaelta <iacusia.kae@firemail.de>
2023-11-18 23:13:48 +0000

    NO WAY I FIXED IT

--- src/akiko.cr
+++ src/akiko.cr
@@ -1,4 +1,15 @@
-# TODO: Write documentation for `Akiko`
+#
+#   Akiko
+#   -----------
+#   Akiko is a multi-engine bot runner for lichess.
+#   Its spec requires it to do the following:
+#   - Validate engine moves
+#   - Spawn multiple bot instances
+#   - Manage their connections to Lichess
+#   - Allow bots to play games with humans
+#   - Accept new games
+#   - Provide stats about their win/loss rate in a graph
+#
 
 require "http/client"
 require "json"
